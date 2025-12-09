8fbc8dbca2a1b9cdc8949a9038a48f95dcec99ae
Andrew Ruvinsky <andrewruvinsky@gmail.com>
2025-12-08 17:10:17 -0800

    implment SimpleANN class

---
+++ src/SimpleANN.h
@@ -0,0 +1,27 @@
+// src/SimpleANN.h
+
+#ifndef SIMPLE_ANN_H
+#define SIMPLE_ANN_H
+#include <Eigen/Dense>
+
+using MatrixFloat = Eigen::MatrixXf;
+using VectorInt = Eigen::VectorXi;
+
+// Helper functions
+MatrixFloat softmax(const MatrixFloat &logits);
+float crossEntropyLoss(const MatrixFloat &predictions, const MatrixFloat &targets);
+float computeAccuracy(const MatrixFloat &predictions, const VectorInt &trueLabels);
+
+class SimpleANN {
+public:
+    MatrixFloat weightsInputToHidden;
+    Eigen::VectorXf biasHidden;
+    MatrixFloat weightsHiddenToOutput;
+    Eigen::VectorXf biasOutput;
+
+    SimpleANN(int numFeatures, int numHiddenUnits, int numClasses);
+    MatrixFloat predict(const MatrixFloat& images);
+    void train(const MatrixFloat& trainImages, const MatrixFloat& trainLabelsOneHot, int numEpochs, float learningRate, int batchSize);
+};
+
+#endif
